624a5fbf882457cfe208bdccb533ffd9d87cc121
Lorety-VL <vladimirking18@gmail.com>
2022-12-11 20:45:11 +0300

    brain-gcd-logic refactoring

--- src/games/brain-gcd-logic.js
+++ src/games/brain-gcd-logic.js
@@ -11,18 +11,12 @@ const gameGcd = () => {
     const questNums = `${number1} ${number2}`;
 
     const getGcd = (numberLocal1, numberLocal2) => {
-      let num1 = numberLocal1;
-      let num2 = numberLocal2;
-      while (num1 !== 0 && num2 !== 0) {
-        if (num1 > num2) {
-          num1 %= num2;
-        } else {
-          num2 %= num1;
-        }
-      }
-      const gcd = num1 + num2;
-      return (gcd);
-    };
+      if (numberLocal2 === 0) {
+          return numberLocal1
+      } else {
+          return getGcd(numberLocal2, numberLocal1 % numberLocal2)
+      }  
+    }
     let rightAnswer = getGcd(number1, number2);
     rightAnswer = String(rightAnswer);
     return ([questNums, rightAnswer]);
